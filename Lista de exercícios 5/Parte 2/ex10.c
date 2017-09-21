e4fe72ef187a5c3689fb91e902e4a74d866fc3f1
Leonardo de Morais <leonardo.morais@cwi.com.br>
2017-09-21 11:53:19 -0500

    Adicionar exercicios 9 e 10

---
+++ Lista de exercícios 5/Parte 2/ex10.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int load_firefox(char *url) 
+{
+	char argString[500];
+	sprintf(argString, "%s", "firefox --new-window");
+	strcat(argString, " ");
+	strcat(argString, url);
+	return system(argString);
+}
+
+void start_browser(char *url) 
+{
+    pid_t pid;
+    if ((pid = fork()) < 0)
+    {
+        perror("\nProblema na criação do processo filho.\n");
+		exit(1);
+    }
+    else if (pid == 0)
+    {
+        int status = load_firefox(url);
+        exit(status);
+    }
+}
+
+int main(int argc, char *argv[]) 
+{
+    int i;
+    for (i = 1; i < argc; i++) 
+    {
+        start_browser(argv[i]);
+    }
+    printf("Não bloqueou\n");
+	return 0;
+}
---
+++ Lista de exercícios 5/Parte 2/ex9.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+void load_firefox(char *url) 
+{
+	char argString[500];
+	sprintf(argString, "%s", "firefox --new-window");
+	strcat(argString, " ");
+	strcat(argString, url);
+	system(argString);
+}
+
+void start_browser(char *url) 
+{
+    pid_t pid;
+    if ((pid = fork()) < 0)
+    {
+        perror("\nProblema na criação do processo filho.\n");
+		return;
+    }
+    else if (pid == 0)
+    {
+        load_firefox(url);
+    }
+}
+
+int main(int argc, char *argv[]) 
+{
+    start_browser(argv[1]);
+    printf("Não bloqueou\n");
+	return 0;
+}
